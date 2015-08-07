c5ad851db86ccaced28f9b6b81aed28968679243
tysonwhuntley <Huntley@Tysons-Air.fios-router.home>
2015-08-07 13:53:17 -0400

    Initial commit

---
+++ front-page.php
@@ -0,0 +1,333 @@
+
+<div class="container">
+	<div class="row">
+		<div class="col-sm-12">
+			<h1>slider</h1>
+		</div>
+	</div>
+</div>
+
+<!-- section start -->
+<!-- ================ -->
+<section class="section clearfix quote">
+<div class="container">
+  <div class="row">
+    <div class="col-md-12">
+      <div class="call-to-action">
+        <div class="row">
+          <div class="col-sm-8">
+            <h3 class="mt-10 text-muted quote_text">Cost of Web &amp; Mobile Development</h3>
+            <p class="quote_info">Use our form to estimate the intial cost of web and mobile development</p>
+          </div>
+          <div class="col-sm-4 text-right quote_button">
+            <a href="#" class="btn btn-primary btn-lg">Request A Quote <span class="glyphicon glyphicon-send" aria-hidden="true"></span></a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</section>
+<!-- section end -->
+
+<!-- section start -->
+<!-- ================ -->
+<section class="pv-30 clearfix">
+<div class="container">
+  <div class="row">
+    <div class="col-md-12">
+
+      <h2 class="section-title">Our Clients</h2>
+      <div class="separator-2"></div>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. At distinctio quia, et natus nulla cumque consequuntur, <br> sed, quam aliquam excepturi ea necessitatibus facilis, vero illum dignissimos eligendi quasi consectetur possimus.</p>
+
+      <div class="clients-container">
+        <div class="clients">
+
+          <div class="col-sm-3">
+ 				<img src="http://placehold.it/300x300">
+ 			</div>
+ 			<div class="col-sm-3">
+ 				<img src="http://placehold.it/300x300">
+ 			</div>
+ 			<div class="col-sm-3">
+ 				<img src="http://placehold.it/300x300">
+ 			</div>
+ 			<div class="col-sm-3">
+ 				<img src="http://placehold.it/300x300">
+ 			</div>
+        
+        </div>
+      </div>
+
+    </div>
+  </div>
+</div>
+</section>
+<!-- section end -->
+
+<!-- SERVICES -->
+<section>
+<div class="container">
+<div class="row">
+<div class="col-md-12">
+<!-- SERVICES TITLE -->
+<h2 class="section-title">Services</h2>
+<div class="separator-2"></div>
+<p>The <b class="accent-highlight">#1 Mobile App</b> &amp; <b class="accent-highlight">Mobile Website</b> Creator For Small Businesses! Lorem ipsum dolor sit amet, consectetur adipisicing elit.<br>At distinctio quia, et natus nulla cumque consequuntur,  sed, quam aliquam excepturi ea necessitatibus.</p>
+</div>
+</div>
+<!-- /SERVICES TITLE -->
+
+<div class="row mt-20">
+
+<div class="col-sm-6 col-md-3 col-lg-3">
+
+<!-- SERVICES CONTENT BOX -->
+<div class="services-box">
+<div class="services-box-icon">
+<span><img src="<?php echo bloginfo('template_directory'); ?>/assets/images/strategy-3.png"></span>
+</div>
+<h6 class="services-box-title">Branding</h6>
+<div class="services-content-box-text">
+The European languages are members of the same family. Their separate existence is a myth.
+</div>
+</div>
+<!-- /SERVICES CONTENT BOX -->
+
+<!-- SERVICES CONTENT BOX -->
+<div class="services-box">
+<div class="services-box-icon">
+<span><img src="<?php echo bloginfo('template_directory'); ?>/assets/images/hammer2.png"></span>
+</div>
+<h6 class="services-box-title">Development</h6>
+<div class="services-content-box-text">
+The European languages are members of the same family. Their separate existence is a myth.
+</div>
+</div>
+<!-- /SERVICES CONTENT BOX -->
+
+<!-- SERVICES CONTENT BOX -->
+<div class="services-box">
+<div class="services-box-icon">
+<span><img src="<?php echo bloginfo('template_directory'); ?>/assets/images/goal5.png"></span>
+</div>
+<h6 class="services-box-title">Marketing</h6>
+<div class="services-content-box-text">
+The European languages are members of the same family. Their separate existence is a myth.
+</div>
+</div>
+<!-- /SERVICES CONTENT BOX -->
+
+<!-- SERVICES CONTENT BOX -->
+<div class="services-box">
+<div class="services-box-icon">
+<span><img src="<?php echo bloginfo('template_directory'); ?>/assets/images/website8.png"></span>
+</div>
+<h6 class="services-box-title">Design</h6>
+<div class="services-content-box-text">
+The European languages are members of the same family. Their separate existence is a myth.
+</div>
+</div>
+<!-- /SERVICES CONTENT BOX -->
+
+</div>
+
+<div class="col-md-6 col-lg-6 hidden-xs hidden-sm">
+
+<div class="iphone">
+<img src="<?php echo bloginfo('template_directory'); ?>/assets/images/iphone-1white.png" alt="">
+</div>
+
+</div>
+
+<div class="col-sm-6 col-md-3 col-lg-3">
+
+<!-- SERVICES CONTENT BOX -->
+<div class="services-box">
+<div class="services-box-icon">
+<span><img src="<?php echo bloginfo('template_directory'); ?>/assets/images/savings1.png"></span>
+</div>
+<h6 class="services-box-title">Extremely Affordable</h6>
+<div class="services-content-box-text">
+More affordable than a newspaper ad, huge return on investment for businesses.
+</div>
+</div>
+<!-- /SERVICES CONTENT BOX -->
+
+<!-- SERVICES CONTENT BOX -->
+<div class="services-box">
+<div class="services-box-icon">
+<span><img src="<?php echo bloginfo('template_directory'); ?>/assets/images/website24.png"></span>
+</div>
+<h6 class="services-box-title">Powerful Hosting</h6>
+<div class="services-content-box-text">
+Our servers are hosted in the cloud using the latest hosting technology with 99.9% uptime for your apps.
+</div>
+</div>
+<!-- /SERVICES CONTENT BOX -->
+
+<!-- SERVICES CONTENT BOX -->
+<div class="services-box">
+<div class="services-box-icon">
+<span><img src="<?php echo bloginfo('template_directory'); ?>/assets/images/cellphone57.png"></span>
+</div>
+<h6 class="services-box-title">Mobile</h6>
+<div class="services-content-box-text">
+The European languages are members of the same family. Their separate existence is a myth.
+</div>
+</div>
+<!-- /SERVICES CONTENT BOX -->
+
+<!-- SERVICES CONTENT BOX -->
+<div class="services-box">
+<div class="services-box-icon">
+<span><img src="<?php echo bloginfo('template_directory'); ?>/assets/images/cloud-storage.png"></span>
+</div>
+<h6 class="services-box-title">Preview Online</h6>
+<div class="services-content-box-text">
+We provide a free preview of your app online or from your iPhone, iPad, or Android device.
+</div>
+</div>
+<!-- /SERVICES CONTENT BOX -->
+
+</div>
+
+</div>
+
+</div>
+
+</section>
+<!-- /SERVICES SERVICES -->
+
+
+
+<!-- RECENT PROJECTS -->
+<section class="main-container">
+
+        <div class="container">
+          <div class="row">
+
+            <!-- main start -->
+            <!-- ================ -->
+            <div class="main col-md-12">
+
+              <!-- page-title start -->
+              <!-- ================ -->
+              <h2 class="section-title">Recent Projects</h2>
+              <div class="separator-2"></div>
+              <!-- page-title end -->
+              <p class="services-content-box-text">Our process on <b class="accent-highlight">Creating Awesome Projects.</b> Lorem ipsum dolor sit amet.</br>Consectetur adipisicing elit Illo quaerat commodi excepturi dignissimos!</p>
+
+<!-- black asphalt slide begins -->
+                <div class="col-sm-6 col-md-3">
+                  <div class="image-box">
+                    <div class="overlay-container">
+                      <img src="<?php echo bloginfo('template_directory'); ?>/images/client1blackasphalt.png" alt="">
+                    </div>
+                    <div class="body">
+                      <h3>Black Asphalt</h3>
+                      <div class="separator"></div>
+                      <p>The Desert Snow Training Program has provided the law enforcement community with the finest interdiction training.</p>
+                      <a class="btn btn-default" href="#" role="button">Read More</a>
+                    </div>
+                  </div>
+                </div>
+<!-- black asphalt slide ends -->
+
+
+<!-- sycuan casino slide begins -->
+                <div class="col-sm-6 col-md-3 isotope-item mobile-development">
+                  <div class="image-box style-2 mb-20 shadow bordered light-gray-bg text-center">
+                    <div class="overlay-container">
+                      <img src="<?php echo bloginfo('template_directory'); ?>/assets/images/client2sycuancasino.png" alt="">
+                      <div class="overlay-to-top">
+                        <p class="small margin-clear"><em>Mobile Development <br> Sycuan Casino</em></p>
+                      </div>
+                    </div>
+                    <div class="body">
+                      <h3>Sycuan Casino</h3>
+                      <div class="separator"></div>
+                      <p>Contracted to create a website for this San Diego casino. Utilized ExpressionEngine with various plugins.</p>
+                      <a href="portfolio-item.html" class="btn btn-default btn-sm btn-hvr hvr-shutter-out-horizontal margin-clear">Read More<i class="fa fa-arrow-right pl-10"></i></a>
+                    </div>
+                  </div>
+                </div>
+<!-- sycuan casino slide ends -->
+
+
+<!-- femme akoi slide begins -->
+                <div class="col-sm-6 col-md-3 isotope-item web-design">
+                  <div class="image-box style-2 mb-20 shadow bordered light-gray-bg text-center">
+                    <div class="overlay-container">
+                      <img src="<?php echo bloginfo('template_directory'); ?>/assets/images/client5femmeakoi.png" alt="">
+                      <div class="overlay-to-top">
+                        <p class="small margin-clear"><em>Web Design<br>Femme Akoi</em></p>
+                      </div>
+                    </div>
+                    <div class="body">
+                      <h3>Femme Akoi</h3>
+                      <div class="separator"></div>
+                      <p>Created to display the talents and creativity of a local makeup artist, utilizes an eye-catching design.</p>
+                      <a href="portfolio-item.html" class="btn btn-default btn-sm btn-hvr hvr-shutter-out-horizontal margin-clear">Read More<i class="fa fa-arrow-right pl-10"></i></a>
+                    </div>
+                  </div>
+                </div>
+<!-- femme akoi slide ends -->
+
+<!-- non-12Step rehab slide begins -->
+                <div class="col-sm-6 col-md-3 isotope-item mobile-development">
+                  <div class="image-box style-2 mb-20 shadow bordered light-gray-bg text-center">
+                    <div class="overlay-container">
+                      <img src="<?php echo bloginfo('template_directory'); ?>/assets/images/client10non12steprehab.png" alt="">
+                      <div class="overlay-to-top">
+                        <p class="small margin-clear"><em>Mobile Development<br> Non-12Step Rehab</em></p>
+                      </div>
+                    </div>
+                    <div class="body">
+                      <h3>Non-12Step Rehab</h3>
+                      <div class="separator"></div>
+                      <p>This is a smaller type of website that is also optimized to show up in search engine results. A clean and simple design.</p>
+                      <a href="portfolio-item.html" class="btn btn-default btn-sm btn-hvr hvr-shutter-out-horizontal margin-clear">Read More<i class="fa fa-arrow-right pl-10"></i></a>
+                    </div>
+                  </div>
+                </div>
+<!-- non-12Steprehab slide ends -->
+
+            </div>
+            <!-- main end -->
+
+          </div>
+        </div>
+      </section>
+      <!-- main-container end -->
+
+      <!-- section start -->
+      <!-- ================ -->
+      <section class="section pv-40 parallax background-img-1 dark-translucent-bg" style="background-position:50% 60%;">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-12">
+              <div class="call-to-action text-center">
+                <div class="row">
+                  <div class="col-sm-8 col-sm-offset-2">
+                    <h2 class="title">Subscribe to Our Newsletter</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus error pariatur deserunt laudantium nam, mollitia quas nihil inventore, quibusdam?</p>
+                    <div class="separator"></div>
+                    <form class="form-inline margin-clear">
+                      <div class="form-group has-feedback">
+                        <label class="sr-only" for="subscribe2">Email address</label>
+                        <input type="email" class="form-control" id="subscribe2" placeholder="Enter email" name="subscribe2" required="">
+                        <i class="fa fa-envelope form-control-feedback"></i>
+                      </div>
+                      <button type="submit" class="btn btn-gray-transparent btn-animated margin-clear">Submit <i class="fa fa-send"></i></button>
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <div class="clearfix"></div>
+      <!-- section end -->
